42aefe306ffc83bdfea1e41df1e4b63a2bdd50ce
Takeshi Kimata <117462761+kimatata@users.noreply.github.com>
2024-03-22 23:07:16 +0900

    Unify tabs with nextui's ListBoxItem

--- frontend/app/projects/[projectId]/sidebar.tsx
+++ frontend/app/projects/[projectId]/sidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Menu, MenuItem } from "@nextui-org/react";
+import { useState } from "react";
+import { Listbox, ListboxItem } from "@nextui-org/react";
 import { Home, Files, FlaskConical } from "lucide-react";
 import { useRouter } from "next/navigation";
 import useGetCurrentIds from "@/utils/useGetCurrentIds";
@@ -7,31 +8,61 @@ import useGetCurrentIds from "@/utils/useGetCurrentIds";
 export default function Sidebar() {
   const { projectId } = useGetCurrentIds();
   const router = useRouter();
+
+  const [currentKey, setCurrentTab] = useState("home");
+  const listBoxItemClassName = "p-3 rounded-none border-l-4 border-transparent";
+
+  const handleTabClick = (key: string) => {
+    setCurrentTab(key);
+    if (key === "home") {
+      router.push(`/projects/${projectId}/home`);
+    } else if (key === "cases") {
+      router.push(`/projects/${projectId}/folders`);
+    } else if (key === "runs") {
+      router.push(`/projects/${projectId}/runs`);
+    }
+  };
+
+  const tabItems = [
+    {
+      key: "home",
+      text: "Home",
+      startContent: <Home strokeWidth={1} size={28} />,
+    },
+    {
+      key: "cases",
+      text: "Test Cases",
+      startContent: <Files strokeWidth={1} size={28} />,
+    },
+    {
+      key: "runs",
+      text: "Test Runs",
+      startContent: <FlaskConical strokeWidth={1} size={28} />,
+    },
+  ];
+
   return (
     <div className="w-64 border-r-1 dark:border-neutral-700">
-      <Menu aria-label="sidebar">
-        <MenuItem
-          startContent={<Home strokeWidth={1} size={28} />}
-          className="p-3"
-          onClick={() => router.push(`/projects/${projectId}/home`)}
-        >
-          Home
-        </MenuItem>
-        <MenuItem
-          startContent={<Files strokeWidth={1} size={28} />}
-          className="p-3"
-          onClick={() => router.push(`/projects/${projectId}/folders`)}
-        >
-          Test Cases
-        </MenuItem>
-        <MenuItem
-          startContent={<FlaskConical strokeWidth={1} size={28} />}
-          className="p-3"
-          onClick={() => router.push(`/projects/${projectId}/runs`)}
-        >
-          Test Runs
-        </MenuItem>
-      </Menu>
+      <Listbox
+        aria-label="Listbox Variants"
+        className="p-0"
+        onClick={() => router.push(`/projects/${projectId}/home`)}
+      >
+        {tabItems.map((itr, index) => (
+          <ListboxItem
+            key={itr.key}
+            startContent={itr.startContent}
+            onClick={() => handleTabClick(itr.key)}
+            className={
+              currentKey === itr.key
+                ? listBoxItemClassName + " border-gray-600 "
+                : listBoxItemClassName
+            }
+          >
+            {itr.text}
+          </ListboxItem>
+        ))}
+      </Listbox>
     </div>
   );
 }
