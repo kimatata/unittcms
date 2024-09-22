c00e85b8e241cd07ecbd513a23636dbcf2626370
Takeshi Kimata <117462761+kimatata@users.noreply.github.com>
2024-09-22 12:50:17 +0900

    Change version

--- frontend/src/app/[locale]/HeaderNavbarMenu.tsx
+++ frontend/src/app/[locale]/HeaderNavbarMenu.tsx
@@ -94,7 +94,7 @@ export default function HeaderNavbarMenu({ messages, locale }: Props) {
         <NavbarItem className="hidden md:block">
           <Chip size="sm" variant="flat">
             <Link className="data-[active=true]:text-primary data-[active=true]:font-medium" href="/" locale={locale}>
-              1.0.0-beta.6
+              1.0.0-beta.7
             </Link>
           </Chip>
         </NavbarItem>
