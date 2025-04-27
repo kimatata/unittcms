8c620cad341550d88911a363fd48bc9ef4d241ae
Takeshi Kimata <117462761+kimatata@users.noreply.github.com>
2025-04-27 19:20:55 +0900

    feat: e2e test on github actions

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:8000/');
-  await expect(page).toHaveTitle('Open Source Test Case Management System');
+  await expect(page).toHaveTitle('Open Source Test Case Management System | UnitTCMS');
 });
