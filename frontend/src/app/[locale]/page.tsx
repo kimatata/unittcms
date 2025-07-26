ffd5af8e5c5c8363e7d37bd5f4559aa8fc5e098d
kimatata <117462761+kimatata@users.noreply.github.com>
2025-07-26 14:31:01 +0900

    chore: eslint&format check on workflow (#253)

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,13 +1,12 @@
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 import { PageType } from '@/types/base';
 import Config from '@/config/config';
 import LandingPage from '@/src/app/[locale]/LandingPage';
 
 export default function Home({ params }: PageType) {
-
   if (!Config.isDemoSite) {
-    redirect(`/${params.locale}/projects`)
+    redirect(`/${params.locale}/projects`);
   }
 
-  return <LandingPage params={params} />
+  return <LandingPage params={params} />;
 }
