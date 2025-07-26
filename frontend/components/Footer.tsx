7fec97d05ddcde62af2de9c045159eb82d66e270
kimatata <www.iepen@gmail.com>
2025-07-26 15:26:08 +0900

    feat: Footer component

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { Link, NextUiLinkClasses } from '@/src/i18n/routing';
 import { LocaleCodeType } from '@/types/locale';
 
@@ -7,10 +8,18 @@ type Props = {
 
 export default function Footer({ locale }: Props) {
   return (
-    <div className="w-full text-center py-2 px-6 flex flex-wrap justify-center items-center gap-4">
-      <div>Copyright © 2024-present UnitTCMS</div>
+    <div className="w-full text-center py-2 px-6 flex flex-wrap justify-center items-center gap-4 text-gray-500">
+      <Link href="https://unittcms.org">
+        <Image src="/favicon/icon-192.png" width={32} height={32} alt="Logo" />
+      </Link>
+      <div>
+        <span>Copyright © 2024-present </span>
+        <Link href="https://unittcms.org" className={`${NextUiLinkClasses} !text-gray-500 hover:text-gray-700`}>
+          UnitTCMS
+        </Link>
+      </div>
 
-      <Link href={'/health'} locale={locale} className={`${NextUiLinkClasses} text-gray-500 hover:text-gray-700`}>
+      <Link href={'/health'} locale={locale} className={`${NextUiLinkClasses} !text-gray-500 hover:text-gray-700`}>
         Status
       </Link>
     </div>
