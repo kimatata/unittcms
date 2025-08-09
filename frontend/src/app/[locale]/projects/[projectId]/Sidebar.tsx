2a83094cf486c4ed6f74feb5d3aaa2d9d5bb8a3a
kimatata <www.iepen@gmail.com>
2025-08-10 01:18:04 +0900

    update

--- frontend/src/app/[locale]/projects/[projectId]/Sidebar.tsx
+++ frontend/src/app/[locale]/projects/[projectId]/Sidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { Listbox, ListboxItem } from '@heroui/react';
-import { Home, Files, FlaskConical, Users, Settings } from 'lucide-react';
+import { Listbox, ListboxItem, Button, Tooltip } from '@heroui/react';
+import { AlignJustify, Home, Files, FlaskConical, Users, Settings } from 'lucide-react';
 import { usePathname, useRouter } from '@/src/i18n/routing';
 import useGetCurrentIds from '@/utils/useGetCurrentIds';
 import { ProjectMessages } from '@/types/project';
@@ -16,12 +16,15 @@ export default function Sidebar({ messages, locale }: Props) {
   const router = useRouter();
   const pathname = usePathname();
 
-  const [currentKey, setCurrentTab] = useState('home');
+  // const [currentKey, setCurrentTab] = useState('home');
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+  const [selectedKeys, setSelectedKeys] = useState(new Set(['home']));
+  // const selectedValue = useMemo(() => Array.from(selectedKeys).join(', '), [selectedKeys]);
 
   const ICON_STROKE_WIDTH = 2;
   const ICON_SIZE = 16;
-  const baseClass = 'h-18 flex flex-col p-3 text-gray-500 border-l-2 rounded-none border-transparent';
-  const selectedClass = `${baseClass} bg-neutral-200 dark:bg-neutral-700 !text-primary border-primary`;
+  // const baseClass = 'h-12 p-3';
+  // const selectedClass = `${baseClass} bg-neutral-200 dark:bg-neutral-700`;
 
   const handleTabClick = (key: string) => {
     if (key === 'home') {
@@ -40,15 +43,15 @@ export default function Sidebar({ messages, locale }: Props) {
   useEffect(() => {
     const handleRouteChange = (currentPath: string) => {
       if (currentPath.includes('home')) {
-        setCurrentTab('home');
+        setSelectedKeys(new Set(['home']));
       } else if (currentPath.includes('folders')) {
-        setCurrentTab('cases');
+        setSelectedKeys(new Set(['cases']));
       } else if (currentPath.includes('runs')) {
-        setCurrentTab('runs');
+        setSelectedKeys(new Set(['runs']));
       } else if (currentPath.includes('members')) {
-        setCurrentTab('members');
+        setSelectedKeys(new Set(['members']));
       } else if (currentPath.includes('settings')) {
-        setCurrentTab('settings');
+        setSelectedKeys(new Set(['settings']));
       }
     };
 
@@ -59,44 +62,90 @@ export default function Sidebar({ messages, locale }: Props) {
     {
       key: 'home',
       text: messages.home,
-      startContent: <Home strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />,
+      startContent: (
+        <Tooltip hidden={isSideBarOpen} content={messages.home} placement="right">
+          <Home strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />
+        </Tooltip>
+      ),
     },
     {
       key: 'cases',
       text: messages.testCases,
-      startContent: <Files strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />,
+      startContent: (
+        <Tooltip hidden={isSideBarOpen} content={messages.testCases} placement="right">
+          <Files strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />
+        </Tooltip>
+      ),
     },
     {
       key: 'runs',
       text: messages.testRuns,
-      startContent: <FlaskConical strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />,
+      startContent: (
+        <Tooltip hidden={isSideBarOpen} content={messages.testRuns} placement="right">
+          <FlaskConical strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />
+        </Tooltip>
+      ),
     },
     {
       key: 'members',
       text: messages.members,
-      startContent: <Users strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />,
+      startContent: (
+        <Tooltip hidden={isSideBarOpen} content={messages.members} placement="right">
+          <Users strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />
+        </Tooltip>
+      ),
     },
     {
       key: 'settings',
       text: messages.settings,
-      startContent: <Settings strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />,
+      startContent: (
+        <Tooltip hidden={isSideBarOpen} content={messages.settings} placement="right">
+          <Settings strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />
+        </Tooltip>
+      ),
     },
   ];
 
   return (
-    <div className="w-18 border-r-1 dark:border-neutral-700">
-      <Listbox aria-label="Listbox Variants" variant="light" className="p-0">
+    <div className="border-r-1 dark:border-neutral-700">
+      <Listbox
+        disallowEmptySelection
+        aria-label="Listbox Variants"
+        variant="light"
+        selectionMode="single"
+        selectedKeys={selectedKeys}
+        hideSelectedIcon
+        topContent={
+          <Button isIconOnly variant="light" onPress={() => setIsSideBarOpen(!isSideBarOpen)}>
+            <AlignJustify strokeWidth={ICON_STROKE_WIDTH} size={ICON_SIZE} />
+          </Button>
+        }
+        // onSelectionChange={setSelectedKeys}
+        // onSelectionChange={() => handleTabClick(itr.key)}
+      >
         {tabItems.map((itr) => (
           <ListboxItem
             key={itr.key}
             startContent={itr.startContent}
             onPress={() => handleTabClick(itr.key)}
-            className={currentKey === itr.key ? selectedClass : baseClass}
+            className="h-12"
+            // color={currentKey === itr.key ? 'primary' : ''}
+            // className={currentKey === itr.key ? selectedClass : baseClass}
           >
-            {itr.text}
+            {isSideBarOpen ? itr.text : ''}
           </ListboxItem>
         ))}
       </Listbox>
+
+      {/* {tabItems.map((itr) => (
+        <Tooltip content={itr.text} placement="right" key={itr.key}>
+          <Button
+            startContent={itr.startContent}
+            onPress={() => handleTabClick(itr.key)}
+            className={currentKey === itr.key ? selectedClass : baseClass}
+          />
+        </Tooltip>
+      ))} */}
     </div>
   );
 }
