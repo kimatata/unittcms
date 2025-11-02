45b4a24c5a7bb09c9a0cd8b820902c2af2e60317
Matheus Santana <matheusfsantana99@gmail.com>
2025-11-02 09:58:17 -0300

    fix: do not generate all runCases on run creation

--- backend/routes/runs/new.js
+++ backend/routes/runs/new.js
@@ -24,8 +24,6 @@ export default function (sequelize) {
   Case.hasMany(RunCase, { foreignKey: 'caseId' });
 
   router.post('/', verifySignedIn, verifyProjectReporterFromProjectId, async (req, res) => {
-    const t = await sequelize.transaction();
-
     try {
       const projectId = req.query.projectId;
       const { name, configurations, description, state } = req.body;
@@ -33,51 +31,16 @@ export default function (sequelize) {
         return res.status(400).json({ error: 'Name and projectId are required' });
       }
 
-      const newRun = await Run.create(
-        {
-          name,
-          configurations,
-          description,
-          state,
-          projectId,
-        },
-        { transaction: t }
-      );
+      const newRun = await Run.create({
+        name,
+        configurations,
+        description,
+        state,
+        projectId,
+      });
 
-      const projectCases = await Case.findAll(
-        {
-          include: [
-            {
-              model: Folder,
-              where: {
-                projectId: projectId,
-              },
-              attributes: [],
-            },
-          ],
-        },
-        { transaction: t }
-      );
-
-      const runCasesToCreate = [];
-      for (const _case of projectCases) {
-        runCasesToCreate.push({
-          runId: newRun.id,
-          caseId: _case.id,
-          status: 0,
-        });
-      }
-
-      const chunkSize = 100;
-      for (let i = 0; i < runCasesToCreate.length; i += chunkSize) {
-        const chunk = runCasesToCreate.slice(i, i + chunkSize);
-        await RunCase.bulkCreate(chunk, { transaction: t });
-      }
-
-      t.commit();
       res.json(newRun);
     } catch (error) {
-      t.rollback();
       console.error('Error creating new run:', error);
       res.status(500).json({ error: 'Internal server error' });
     }
